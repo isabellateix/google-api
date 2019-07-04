ef5544ec9558a739339ce9bd736e167a0f6ce855
Isabella Teixeira <isabellamteixeira@gmail.com>
2019-07-03 21:53:18 -0300

    map styles

--- js/main.js
+++ js/main.js
@@ -1,14 +1,113 @@
   function initMap() {
-  	const	moodev = {lat: -31.762193, lng: -52.342067}
+  	const	customMap = [
+            {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+            {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+            {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+            {
+              featureType: 'administrative.locality',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'poi',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'poi.park',
+              elementType: 'geometry',
+              stylers: [{color: '#263c3f'}]
+            },
+            {
+              featureType: 'poi.park',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#6b9a76'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'geometry',
+              stylers: [{color: '#38414e'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'geometry.stroke',
+              stylers: [{color: '#212a37'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#9ca5b3'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'geometry',
+              stylers: [{color: '#746855'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'geometry.stroke',
+              stylers: [{color: '#1f2835'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#f3d19c'}]
+            },
+            {
+              featureType: 'transit',
+              elementType: 'geometry',
+              stylers: [{color: '#2f3948'}]
+            },
+            {
+              featureType: 'transit.station',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'geometry',
+              stylers: [{color: '#17263c'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#515c6d'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'labels.text.stroke',
+              stylers: [{color: '#17263c'}]
+            }
+          ];
+
+  	const	moodev = {lat: -31.762193, lng: -52.342067};
+
+  	const image = {
+  		url:'https://source.unsplash.com/random/tomato',
+  		size: new google.maps.Size(100, 100),
+  	}; 
+
 
     const map = new google.maps.Map(document.getElementById('map'), {
       center: moodev,
-      zoom: 18
+      zoom: 18,
+      styles: customMap,
     });
 
     const marker = new google.maps.Marker({
 	    position: moodev,
 	    map: map,
-	    title:"Moodev!"
+	    title:'Moodev!',
+	    icon: image,
 		});
+
+    const textoJanela = '<p>Olá, aqui é a Moodev</p>';
+		const infoWindow = new google.maps.InfoWindow({
+    	content: textoJanela,
+  	});
+
+		marker.addListener('click', function(){
+			infoWindow.open(map, marker);
+		});
+
   }
